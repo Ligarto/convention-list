ee7e942ce2d45fcd98e316d06c629cdd15793bd5
Ligarto <ligartogamer@gmail.com>
2026-02-05 14:44:20 +0000

    removed some bloat and fixed TSL link

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,6 @@
     <navbar></navbar>
 
     <router-view></router-view>
-
-
-
-    <!--
-    <page-viewer 
-        v-if="pages.length > 0"
-        :page="pages[activePage]" 
-    ></page-viewer>
-
-    <create-page
-        @page-created="pageCreated"
-    ></create-page>
-    -->
-
-    <!--<div v-if="activePage == 0">
-        <ol>
-            <li v-for="(level, index) in levels" class="container" :key="index">
-                <a href="#">{{ level.name }}</a>
-            </li>
-        </ol>
-    </div>-->
 </template>
 
 <script>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,4 @@
 import {createRouter, createWebHashHistory} from  'vue-router'
-import CreatePage from './views/CreatePage.vue'
-import Pages from './views/Pages.vue'
-import PagesList from './views/PagesList.vue'
-import PageEdit from './views/PageEdit.vue'
 import List from './views/List.vue'
 import HomePage from './views/HomePage.vue'
 import BawlZapher from './views/BawlZapher.vue'
